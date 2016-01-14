08388f83d19d397f4462274beaf2920ba5e9cc02
Blake Hagen <blakehagen@gmail.com>
2016-01-13 19:50:30 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('listening on port' + port);
+});
