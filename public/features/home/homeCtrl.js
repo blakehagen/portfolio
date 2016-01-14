ddeed76d8fff67538ed21d8ccda7dbc9d5964881
Blake Hagen <blakehagen@gmail.com>
2016-01-13 23:00:10 -0700

    tab toggle working

--- public/features/home/homeCtrl.js
+++ public/features/home/homeCtrl.js
@@ -1,6 +1,61 @@
-angular.module('portfolio').controller('homeCtrl', function($scope){
-    
-    
-    $scope.test = 'HELLO WORLD!'
-    
+angular.module('portfolio').controller('homeCtrl', function ($scope) {
+
+    // TAB TOGGLES //
+    $scope.activeTab1 = true;
+    $scope.activeTab2 = false;
+    $scope.activeTab3 = false;
+    $scope.activeTab4 = false;
+    $scope.activeTab5 = false;
+    $scope.activeTab6 = false;
+
+    $scope.toggleTab1 = function () {
+        $scope.activeTab1 = true;
+        $scope.activeTab2 = false;
+        $scope.activeTab3 = false;
+        $scope.activeTab4 = false;
+        $scope.activeTab5 = false;
+        $scope.activeTab6 = false;
+    }
+    $scope.toggleTab2 = function () {
+        $scope.activeTab1 = false;
+        $scope.activeTab2 = true;
+        $scope.activeTab3 = false;
+        $scope.activeTab4 = false;
+        $scope.activeTab5 = false;
+        $scope.activeTab6 = false;
+    }
+    $scope.toggleTab3 = function () {
+        $scope.activeTab1 = false;
+        $scope.activeTab2 = false;
+        $scope.activeTab3 = true;
+        $scope.activeTab4 = false;
+        $scope.activeTab5 = false;
+        $scope.activeTab6 = false;
+    }
+    $scope.toggleTab4 = function () {
+        $scope.activeTab1 = false;
+        $scope.activeTab2 = false;
+        $scope.activeTab3 = false;
+        $scope.activeTab4 = true;
+        $scope.activeTab5 = false;
+        $scope.activeTab6 = false;
+    }
+    $scope.toggleTab5 = function () {
+        $scope.activeTab1 = false;
+        $scope.activeTab2 = false;
+        $scope.activeTab3 = false;
+        $scope.activeTab4 = false;
+        $scope.activeTab5 = true;
+        $scope.activeTab6 = false;
+    }
+    $scope.toggleTab6 = function () {
+        $scope.activeTab1 = false;
+        $scope.activeTab2 = false;
+        $scope.activeTab3 = false;
+        $scope.activeTab4 = false;
+        $scope.activeTab5 = false;
+        $scope.activeTab6 = true;
+    }
+
+
 });
