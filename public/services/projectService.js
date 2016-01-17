db4d6324b02b0fa2af4c29a0d31be06285ff3d46
Blake Hagen <blakehagen@gmail.com>
2016-01-16 18:18:10 -0700

    font weight adjustments on proj view

--- public/services/projectService.js
+++ public/services/projectService.js
@@ -10,7 +10,8 @@ angular.module('portfolio').service('projService', function ($q) {
                 source_code_url: 'https://goo.gl/ONpAhj',
                 description: 'Stay up to date with information from top European soccer leagues. See league standings, upcoming fixtures, results, and leading goal scorers for the English Premier League, German Bundesliga, Spanish Primera Dvision, UEFA Champions League, & others.',
                 tech: 'Built with AngularJS, CSS, HTML. JavaScript used to maniputlate data to display. Also built a separate server with Node.js and Express to make API calls to gather soccer data for the application.',
-                img_urls: ['/styles/assets/screenshots/fw/fw1.jpg', '/styles/assets/screenshots/fw/fw2.jpg']
+                img_urls: ['/styles/assets/screenshots/fw/fw1.jpg', '/styles/assets/screenshots/fw/fw2.jpg'],
+                built: 'November 2015'
             }
         }
         
@@ -21,8 +22,9 @@ angular.module('portfolio').service('projService', function ($q) {
                 live_url: 'http://groopscoop.com/',
                 source_code_url: 'https://goo.gl/lzkaRS',
                 description: 'GroopScoop is a real-time group chat/forum application. Log in with Google, create groups and invite other users to your groups. Chat in real time, or post a message/video/image and come back later to check responses.',
-                tech: 'My official Personal Project during my time at <a href="https://devmounta.in/" target="_blank">DevMountain</a> (completed course January 2016). Real time capability powered by Socket.io. Learned Socket.io outside of the DevMountain curriculum. GroopScoop is built on Angular, Node, Express, Mongoose, and MongoDB.',
-                img_urls: ['/styles/assets/screenshots/gs/gs1.jpg', '/styles/assets/screenshots/gs/gs2.jpg']
+                tech: 'My official Personal Project while at <a href="https://devmounta.in/" target="_blank">DevMountain</a> (completed course January 2016). Real time capability by Socket.io. Learned Socket.io outside of DevMountain curriculum. GroopScoop is built on Angular, Node, Express, Mongoose, and MongoDB.',
+                img_urls: ['/styles/assets/screenshots/gs/gs1.jpg', '/styles/assets/screenshots/gs/gs2.jpg'],
+                built: 'December 2015'
             }
         }
         
@@ -34,7 +36,8 @@ angular.module('portfolio').service('projService', function ($q) {
                 source_code_url: 'https://goo.gl/P1xdze',
                 description: 'To-do web application. Because every portfolio needs a to-do app! Set tasks due by end of day, week, or month. Application calculates in relative time how much time is remaining for you to complete the task.',
                 tech: 'Learned and experimented with Moment.js for relative time and calendar time. Built on Angular, Node, Express, Mongoose, & MongoDB. Passport authentication used for login and clean/simple UI with my own CSS.',
-                img_urls: ['/styles/assets/screenshots/tc/tc1.jpg', '/styles/assets/screenshots/tc/tc2.jpg']
+                img_urls: ['/styles/assets/screenshots/tc/tc1.jpg', '/styles/assets/screenshots/tc/tc2.jpg'],
+                built: 'January 2016'
             }
         }
         
@@ -46,7 +49,8 @@ angular.module('portfolio').service('projService', function ($q) {
                 source_code_url: 'https://goo.gl/tc1Ah5',
                 description: 'Map your current location and see locations of other locationCheckr users you have connected with. You can decide to share or stop sharing your locations with the click of a button.',
                 tech: 'Learned to use the Google Maps API with this project. Learned geolocation and Google Maps reverse geolocation. Real-time map updates with Socket.io. Built on Angular, Node, Express, Mongoose, & MongoDB. User authentication with Passport.',
-                img_urls: ['/styles/assets/screenshots/lc/lc1.jpg', '/styles/assets/screenshots/lc/lc2.jpg']
+                img_urls: ['/styles/assets/screenshots/lc/lc1.jpg', '/styles/assets/screenshots/lc/lc2.jpg'],
+                built: 'December 2015'
             }
         }
         
@@ -56,9 +60,10 @@ angular.module('portfolio').service('projService', function ($q) {
                 icon: '<i class="fa fa-cutlery"></i>',
                 live_url: 'http://45.55.33.203/',
                 source_code_url: 'https://goo.gl/UNY6dQ',
-                description: 'Mobile application (available on iOS and Android) displaying food trucks near your current location. When hunger hits, see if your favorite food truck is nearby.',
+                description: 'Mobile application (available on iOS and Android) displaying food trucks near your current location.',
                 tech: '<a href="https://devmounta.in/" target="_blank">DevMountain</a> group project built using Ionic. Worked with 4 other team members. Learned Ionic outside of DevMountain class time. My responsibilities included incorporating the Google Maps API, real-time truck location sharing using Socket.io, & overall troubleshooting and bug fixing across multiple parts of the project. Learned GitHub in a team setting.',
-                img_urls: ['/styles/assets/screenshots/ftf/ftf1.jpg', '/styles/assets/screenshots/ftf/ftf2.jpg']
+                img_urls: ['/styles/assets/screenshots/ftf/ftf1.jpg', '/styles/assets/screenshots/ftf/ftf2.jpg'],
+                built: 'January 2016'
             }
         }
         
@@ -70,7 +75,8 @@ angular.module('portfolio').service('projService', function ($q) {
                 source_code_url: 'https://goo.gl/tkom3C',
                 description: 'Simple blackjack card game application. See how many hands you can play without going broke while maximizing your cash (not a real gambling site!). Good luck and have fun!',
                 tech: 'Built with JavaScript, jQuery and my own CSS. I wanted to learn how to use jQuery better so I built this game as a short project during my time as a full-time web-dev student at <a href="https://devmounta.in/" target="_blank">DevMountain</a> (completed course January 2016).',
-                img_urls: ['/styles/assets/screenshots/b/b1.jpg', '/styles/assets/screenshots/b/b2.jpg']
+                img_urls: ['/styles/assets/screenshots/b/b1.jpg', '/styles/assets/screenshots/b/b2.jpg'],
+                built: 'November 2015'
             }
         }
         
@@ -82,7 +88,8 @@ angular.module('portfolio').service('projService', function ($q) {
                 source_code_url: 'https://goo.gl/jfpbtf',
                 description: 'Use Fitstantly to instantly see your Fitbit steps and "Very Active Minutes" for the last 7 days. Designed to help you compete against yourself – in the short-term.',
                 tech: 'Fitstantly is my first complete front-end web application and was my personal project during my time as a <a href="https://devmounta.in/" target="_blank">DevMountain</a> student in the web-dev after hours course (part-time - completed March 2015). Built on Angular and uses the Fitbit API to display the user\'s activity data.',
-                img_urls: ['/styles/assets/screenshots/f/f1.jpg', '/styles/assets/screenshots/f/f2.jpg']
+                img_urls: ['/styles/assets/screenshots/f/f1.jpg', '/styles/assets/screenshots/f/f2.jpg'],
+                built: 'March 2015'
             }
         }
     }
