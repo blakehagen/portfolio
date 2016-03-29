42133e75dd90f3b1e4828ec95e8535857640d578
Blake Hagen <blakehagen@gmail.com>
2016-03-28 21:43:25 -0600

    parallax setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,19 @@
 angular.module('portfolio', ['ui.router', 'ngSanitize']).config(function ($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: './features/home/homeTmpl.html',
-            controller: 'homeCtrl as home'
-        })
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: './features/home/homeTmpl.html',
+      controller: 'homeCtrl as home'
+    })
 
-    $urlRouterProvider
-        .otherwise('/');
+    .state('new', {
+      url: '/new',
+      templateUrl: './features/newHome/newHomeTmpl.html',
+      controller: 'homeCtrl as home'
+    })
 
-
-
+  $urlRouterProvider
+    .otherwise('/');
 
 });
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('./public'));
 
 
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('listening on port ' + port);
 });
